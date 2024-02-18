1eb0ae7c16283c7d1f7b6b1e2fc7b74d1334af7c
Ralph Urlus <rurlus.dev@gmail.com>
2024-02-18 17:37:25 +0100

    CHG: Expose OpenMP support flag

--- src/diptest/__init__.py
+++ src/diptest/__init__.py
@@ -4,7 +4,8 @@ import importlib.metadata
 
 from diptest.diptest import dipstat, diptest
 from diptest.lib import _diptest_core as _diptest
+from diptest.lib._diptest_core import _has_openmp_support
 
 __version__ = importlib.metadata.version("diptest")
 
-__all__ = ["dipstat", "diptest", "_diptest", "__version__"]
+__all__ = ["dipstat", "diptest", "_diptest", "_has_openmp_support", "__version__"]
